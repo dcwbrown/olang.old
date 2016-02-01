0a8ac30b89a4b75a00acf6a3ab30f78cf2842c59
David <dave@brownsmeet.com>
2016-02-01 17:28:59 +0000

    Further build simplification

--- compiler/src/EstablishParameters.c
+++ compiler/src/EstablishParameters.c
@@ -1,6 +1,6 @@
 // Generate platform specific files:
-//   voc.par - Oberon compiler type sizes and alignments
-//   Configuration.Mod - version and directory information to build into the compiler
+//   BasicTypeParameters - Oberon compiler type sizes and alignments
+//   Configuration.Mod   - version and directory information to build into the compiler
 // Also tests validity of SYSTEM.h macros on this platform.
 // from vocparam.c originally by J. Templ 23.6.95
 
@@ -23,23 +23,27 @@
 #endif
 
 #define macrotostringhelper(s) #s
-#define macrotostring(s)  macrotostringhelper(s)
+#define macrotostring(s) macrotostringhelper(s)
 
 void fail(char *msg) {printf("%s\n", msg); exit(1);}
 
 
 struct utsname sys;
 
-char  builddate[64];
-char* platform   = "unknown";
-char* olangdirln = macrotostring(OLANG_ROOT) "/olang";
-char* olangdir   = macrotostring(OLANG_ROOT) "/olang-" macrotostring(OLANG_VERSION);
-char* version    = macrotostring(OLANG_VERSION);
-char* cpuarch    = "unknown";
-char* osarch     = "unknown";
-char* ccomp      = "unknown";
-char* cc         = macrotostring(OLANG_CC);
-char* binext     = "";
+char  builddate[256];
+char  olangname[256];
+char  builddir[256];
+char  flavour[256];
+char  prefix[256];
+
+char* platform = "unknown";
+char* prefixln = macrotostring(OLANG_ROOT) "/olang";
+char* version  = macrotostring(OLANG_VERSION);
+char* cpuarch  = "unknown";
+char* osarch   = "unknown";
+char* ccomp    = "unknown";
+char* cc       = macrotostring(OLANG_CC);
+char* binext   = "";
 
 
 void computeParameters() {
@@ -70,10 +74,17 @@ void computeParameters() {
   #else
   ccomp = "unknown";
   #endif
+
+  sprintf(flavour,   "%s.%s.%s",   ccomp, osarch, cpuarch);
+  sprintf(builddir,  "build.%s",   flavour);
+  sprintf(olangname, "olang.%s%s", flavour, binext);
+  sprintf(prefix,    "%s-%s",      prefixln, version);
 }
 
 
 
+
+// Determination of basic type parameters - size, alignment and endianness.
 
 struct {CHAR ch; CHAR x;} c;
 struct {CHAR ch; BOOLEAN x;} b;
@@ -94,11 +105,12 @@ struct {CHAR ch;} rec0;
 struct {CHAR ch; LONGREAL x;} rec1;
 struct {char x[65];} rec2;
 
-long x = 1;
-
-void writeCompilationParameters() {
-  FILE *fd = fopen("voc.par", "w");
-  if (fd == NULL) fail("Couldn't create voc.par.");
+void writeBasicTypeParameters() {
+  char fn[256];
+  strcpy(fn, builddir); 
+  strcat(fn, "/BasicTypeParameters");
+  FILE *fd = fopen(fn, "w");
+  if (fd == NULL) fail("Couldn't create BasicTypeParameters.");
 
   /* get size and alignment of standard types */
   fprintf(fd, "CHAR %lu %lu\n",         (long)sizeof(CHAR),         (long)((char*)&c.x  - (char*)&c));
@@ -112,11 +124,8 @@ void writeCompilationParameters() {
   fprintf(fd, "PTR %lu %lu\n",          (long)sizeof(p.x),          (long)((char*)&p.x  - (char*)&p));
   fprintf(fd, "PROC %lu %lu\n",         (long)sizeof(f.x),          (long)((char*)&f.x  - (char*)&f));
   fprintf(fd, "RECORD %d %lu\n",        (sizeof(rec2)==65) == (sizeof(rec0)==1), (long)(sizeof(rec2)-64));
+  long x = 1;
   fprintf(fd, "ENDIAN %hhd %d\n",       *(char*)&x, 0);
-  fprintf(fd, "SYSTEM.INT8 %lu %lu\n",  (long)sizeof(SYSTEM_INT8),  (long)((char*)&i8.x  - (char*)&i8));
-  fprintf(fd, "SYSTEM.INT16 %lu %lu\n", (long)sizeof(SYSTEM_INT16), (long)((char*)&i16.x - (char*)&i16));
-  fprintf(fd, "SYSTEM.INT32 %lu %lu\n", (long)sizeof(SYSTEM_INT32), (long)((char*)&i32.x - (char*)&i32));
-  fprintf(fd, "SYSTEM.INT64 %lu %lu\n", (long)sizeof(SYSTEM_INT64), (long)((char*)&i64.x - (char*)&i64));
 
   fclose(fd);
 }
@@ -128,8 +137,8 @@ void testPlatformSupport() {
   if (sizeof(CHAR)    != 1)          printf("error: CHAR should have size 1\n");
   if (sizeof(BOOLEAN) != 1)          printf("error: BOOLEAN should have size 1\n");
   if (sizeof(SHORTINT)!= 1)          printf("error: SHORTINT should have size 1\n");
-  if (sizeof(long)    != sizeof p.x) printf("error: LONGINT should have the same size as pointers\n");
-  if (sizeof(long)    != sizeof f.x) printf("error: LONGINT should have the same size as function pointers\n");
+  if (sizeof(LONGINT)  < sizeof p.x) printf("error: LONGINT should have at least the same size as pointers\n");
+  if (sizeof(LONGINT)  < sizeof f.x) printf("error: LONGINT should have at least the same size as function pointers\n");
 
   if (((sizeof(rec2)==65) == (sizeof(rec0)==1)) && ((sizeof(rec2)-64) != sizeof(rec0)))
     printf("error: unsupported record layout  sizeof(rec0) = %lu  sizeof(rec2) = %lu\n", (long)sizeof(rec0), (long)sizeof(rec2));
@@ -142,7 +151,7 @@ void testPlatformSupport() {
   if (__ASHR(2, 1)  !=  1) printf("error: ASH(2, 1) #  1\n");
 
   /* test the __SETRNG macro */
-  x = 0;
+  long x = 0;
   long y = sizeof(SET)*8 - 1;
   if (__SETRNG(x, y) != -1) printf("error: SETRNG(0, MAX(SET)) != -1\n");
 
@@ -162,12 +171,15 @@ void writeMakeParameters() {
   if (fd == NULL) fail("Couldn't create make.include.");
 
   fprintf(fd, "BUILDDATE = %s\n", builddate);
-  fprintf(fd, "PLATFORM  = %s\n", platform);
-  fprintf(fd, "CPUARCH   = %s\n", cpuarch);
-  fprintf(fd, "OSARCH    = %s\n", osarch);
   fprintf(fd, "CCOMP     = %s\n", ccomp);
-  fprintf(fd, "PREFIXLN  = %s\n", olangdirln);
-  fprintf(fd, "PREFIX    = %s\n", olangdir);
+  fprintf(fd, "OSARCH    = %s\n", osarch);
+  fprintf(fd, "CPUARCH   = %s\n", cpuarch);
+  fprintf(fd, "PLATFORM  = %s\n", platform);
+  fprintf(fd, "PREFIX    = %s\n", prefix);
+  fprintf(fd, "PREFIXLN  = %s\n", prefixln);
+  fprintf(fd, "FLAVOUR   = %s\n", flavour);
+  fprintf(fd, "BUILDDIR  = %s\n", builddir);
+  fprintf(fd, "OLANGNAME = %s\n", olangname);
   fprintf(fd, "BINEXT    = %s\n", binext);
 
   fclose(fd);
@@ -177,20 +189,38 @@ void writeMakeParameters() {
 
 
 void writeConfigurationMod() {
-  FILE *fd = fopen("Configuration.Mod", "w");
+  char fn[256];
+  strcpy(fn, builddir); 
+  strcat(fn, "/Configuration.Mod");
+  FILE *fd = fopen(fn, "w");
   if (fd == NULL) fail("Couldn't create Configuration.Mod.");
 
   fprintf(fd, "MODULE Configuration;\n");
   fprintf(fd, "CONST\n");
-  fprintf(fd, "  prefix*      = '%s';\n",   olangdir);
+  fprintf(fd, "  prefix*      = '%s';\n",   prefix);
   fprintf(fd, "  osarch*      = '%s';\n",   osarch);
   fprintf(fd, "  cpuarch*     = '%s';\n",   cpuarch);
   fprintf(fd, "  date*        = '[%s]';\n", builddate);
   fprintf(fd, "  cc*          = '%s';\n",   cc);
   fprintf(fd, "  compiler*    = '%s';\n",   ccomp);
   fprintf(fd, "  version*     = '%s';\n",   version);
-  fprintf(fd, "  versionLong* = 'Oberon compiler olang for %s %s %s [%s] %s';\n",
-    osarch, cpuarch, ccomp, builddate, olangdir);
+  fprintf(fd, "  versionLong* = 'Oberon compiler olang %s [%s] for %s %s using %s in %s';\n\n",
+    version, builddate, osarch, cpuarch, ccomp, prefix);
+
+  fprintf(fd, "  CharSize*    = %1lu;  CharAlign*    = %lu;\n", (long)sizeof(CHAR),         (long)((char*)&c.x  - (char*)&c));
+  fprintf(fd, "  BoolSize*    = %1lu;  BoolAlign*    = %lu;\n", (long)sizeof(BOOLEAN),      (long)((char*)&b.x  - (char*)&b));
+  fprintf(fd, "  SIntSize*    = %1lu;  SIntAlign*    = %lu;\n", (long)sizeof(SHORTINT),     (long)((char*)&si.x - (char*)&si));
+  fprintf(fd, "  IntSize*     = %1lu;  IntAlign*     = %lu;\n", (long)sizeof(INTEGER),      (long)((char*)&i.x  - (char*)&i));
+  fprintf(fd, "  LIntSize*    = %1lu;  LIntAlign*    = %lu;\n", (long)sizeof(LONGINT),      (long)((char*)&li.x - (char*)&li));
+  fprintf(fd, "  SetSize*     = %1lu;  SetAlign*     = %lu;\n", (long)sizeof(SET),          (long)((char*)&s.x  - (char*)&s));
+  fprintf(fd, "  RealSize*    = %1lu;  RealAlign*    = %lu;\n", (long)sizeof(REAL),         (long)((char*)&r.x  - (char*)&r));
+  fprintf(fd, "  LRealSize*   = %1lu;  LRealAlign*   = %lu;\n", (long)sizeof(LONGREAL),     (long)((char*)&lr.x - (char*)&lr));
+  fprintf(fd, "  PointerSize* = %1lu;  PointerAlign* = %lu;\n", (long)sizeof(p.x),          (long)((char*)&p.x  - (char*)&p));
+  fprintf(fd, "  ProcSize*    = %1lu;  ProcAlign*    = %lu;\n", (long)sizeof(f.x),          (long)((char*)&f.x  - (char*)&f));
+  fprintf(fd, "  RecSize*     = %01d;  RecAlign*     = %lu;\n", (sizeof(rec2)==65) == (sizeof(rec0)==1), (long)(sizeof(rec2)-64));
+  long x = 1;
+  fprintf(fd, "  ByteOrder*   = %hhd;  BitOrder*     = %d;\n",  *(char*)&x, 0);
+
   fprintf(fd, "END Configuration.\n");
 
   fclose(fd);
@@ -203,7 +233,8 @@ int main()
 {
   computeParameters();
   testPlatformSupport();
-  writeCompilationParameters();
+  mkdir(builddir, 0744);
+  writeBasicTypeParameters();
   writeConfigurationMod();
   writeMakeParameters();
 }
